0989b33fff4872569bb5bb07b7cf0e8e316bab33
Muhammad Usman <37004406+Usman2907@users.noreply.github.com>
2024-10-27 02:28:10 +0000

    Update enetrypoint.sh file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Download UK map only if it hasn't been downloaded already
-if [ ! -f "/photon/photon_data/uk.osm.pbf" ]; then
+if [ ! -f "/photon/photon_data/united-kingdom-latest.osm.pbf" ]; then
     echo "Downloading UK map..."
 
     # Download UK map
-    wget -O /photon/photon_data/uk.osm.pbf https://download.geofabrik.de/europe/great-britain-latest.osm.pbf
+    wget -O /photon/photon_data/united-kingdom-latest.osm.pbf,.pbf https://download.geofabrik.de/europe/united-kingdom-latest.osm.pbf
 fi
 
 # Start Photon with the UK map file
-if [ -f "/photon/photon_data/uk.osm.pbf" ]; then
+if [ -f "/photon/photon_data/united-kingdom-latest.osm.pbf," ]; then
     echo "Starting Photon with UK map data..."
-    java -jar photon.jar -data-dir /data/photon_data -nominatim-import /photon/photon_data/uk.osm.pbf
+    java -jar photon.jar -data-dir /data/photon_data -nominatim-import /photon/photon_data/united-kingdom-latest.osm.pbf,
 else
     echo "Could not start Photon, the UK map file was not found."
     exit 1
